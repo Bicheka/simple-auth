4468af3391914a455bbe20a4d96c67da8d2b4f96
bicheka <davidmartinezgilp@gmail.com>
2026-01-04 20:41:43 -0500

    added some scripts for drizzle and configured migrations output directory

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
+  out: "./src/db/migrations",
   dialect: "postgresql",
   schema: "./src/db/schema.ts",
   casing: "snake_case",
